3e22d3d565a2dcb0a601b686d8a1cbfadbd86a27
kizimenko <kizimenko@gmail.com>
2023-06-20 13:26:36 +0700

    Manifest V3 and changes

--- Uncompressed/background.js
+++ Uncompressed/background.js
@@ -1,87 +1,76 @@
-(function() {
-let connection; // Порт для связи с popup
-const requestsAll = {}; // Объект, в котором хранятся все запросы со всех открытых вкладок браузера
-const filterStr = 't4k.json'; // Строка, которую обязательно должен содержать запрос в url
+let connection;
+let requestsAll = {}; // use let instead of const
+const filterStr = 't4k.json';
 
-// Слушатель события отправки запроса
+chrome.runtime.onInstalled.addListener(async () => {
+    Object.keys(requestsAll).forEach(key => delete requestsAll[key]);
+    await chrome.storage.local.set({'dataPersistence': false});
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
-	onRequest,
-	{urls: ['*://*/*']},
-	['requestBody']
+  (details) => {
+    if (details.method === 'POST' && details.url.includes(filterStr)) {
+      let arrayBuffer = details.requestBody.raw[0].bytes;
+      let encodedURI = String.fromCharCode.apply(null, new Uint8Array(arrayBuffer));
+      encodedURI = decodeURIComponent(escape(encodedURI));
+      let postedString = decodeURIComponent(encodedURI);
+      let postedObj = JSON.parse(postedString);
+      writeNewRequest(details.tabId, postedObj);
+    }
+  },
+  {urls: ['<all_urls>']},
+  ['requestBody']
 );
 
-// Слушатель и обработчик события перезагрузки вкладки
-chrome.tabs.onUpdated.addListener(function(id, info, tab) {
-    if (info.status === 'loading' && requestsAll.hasOwnProperty(id)) {
-        chrome.storage.local.get('dataPersistence', function(data) {
-            if (!data.dataPersistence) {
-                delete requestsAll[id]; // Удалить данные для перезагруженной вкладки, только если переключатель выключен
-            }
-        });
+chrome.tabs.onUpdated.addListener(async function(id, info, tab) {
+  if (info.status === 'loading' && requestsAll.hasOwnProperty(id)) {
+    let { dataPersistence } = await chrome.storage.local.get('dataPersistence');
+    if (!dataPersistence) {
+      delete requestsAll[id];
     }
+  }
 });
 
-// Слушатель и обработчик события закрытия вкладки
-chrome.tabs.onRemoved.addListener(function(id, info) {
-	// Если в объекте requestsAll хранятся данные для закрытой вкладки
-	if ( requestsAll.hasOwnProperty(id) ) {
-		delete requestsAll[id]; // Удалить данные для закрытой вкладки
-	}
+chrome.tabs.onRemoved.addListener(function(id) {
+  if (requestsAll.hasOwnProperty(id)) {
+    delete requestsAll[id];
+  }
 });
 
-// Слушатель и обработчик события действия браузера
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.browserAction.setPopup({popup: 'popup.html'});
+chrome.action.onClicked.addListener(async function() {
+  let popupUrl = chrome.runtime.getURL('popup.html');
+  let tab = await chrome.tabs.create({url: popupUrl});
 });
 
-
-// Слушатель и обработчик события установления соединения с popup
 chrome.runtime.onConnect.addListener(function(port) {
-	connection = port; // Записать данные соединения в переменную connection
-    connection.onDisconnect.addListener(() => { connection = undefined; }); // При отключении очистить переменную connection
-	// Слушатель и обработчик события получения сообщения
-	port.onMessage.addListener(function(request) {
-		if (request.msg !== 'handshake') return; // Если полученное сообщение не "handshake", завершить функцию
-		// Получить id активной вкладки
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			let id = tabs[0].id;
-			// Отправить ответ
-			let data = requestsAll[id];
-			if ( !requestsAll.hasOwnProperty(id) ) data = []; // Если нет данных для активной вкладки
-			port.postMessage({msg: 'requests', data: data});
-		});
-	});
+  connection = port;
+  connection.onDisconnect.addListener(() => { connection = undefined; });
+
+  port.onMessage.addListener(function(request) {
+    if (request.msg !== 'handshake') return;
+
+    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+      let id = tabs[0].id;
+      let data = requestsAll[id] || [];
+      port.postMessage({msg: 'requests', data: data});
+    });
+  });
 });
 
-// Callback функция при событии отправки запроса
-function onRequest(details) {
-	if ( details.method === 'POST' && details.url.includes(filterStr) ) {
-		let arrayBuffer = details.requestBody.raw[0].bytes;
-		let encodedURI = String.fromCharCode.apply( null, new Uint8Array(arrayBuffer) );
-		encodedURI = decodeURIComponent( escape(encodedURI) ); // Преобразовать ISO-8859-1 в UTF-8
-		let postedString = decodeURIComponent( encodedURI );
-		let postedObj = JSON.parse(postedString);
-		writeNewRequest(details.tabId, postedObj);
-	}
-}
-
-// Функция записи данных нового запроса в объект requestsAll
 function writeNewRequest(tabId, bodyObj) {
-	if ( requestsAll.hasOwnProperty(tabId) ) {
-		let requestsTab = requestsAll[tabId];
-		requestsTab[requestsTab.length] = bodyObj;
-	} else {
-		requestsAll[tabId] = [bodyObj]; // Добавить объект в массив запросов для указанной вкладки
-	}
+  if (requestsAll.hasOwnProperty(tabId)) {
+    let requestsTab = requestsAll[tabId];
+    requestsTab[requestsTab.length] = bodyObj;
+  } else {
+    requestsAll[tabId] = [bodyObj];
+  }
 
-	let reqCount = requestsAll[tabId].length; // Количество выполненных запросов
+  let reqCount = requestsAll[tabId].length;
 
-	// Установить значок для действия браузера с количеством выполненных запросов
-	chrome.browserAction.setBadgeText({tabId: tabId, text: reqCount.toString()}, () => {});
-	chrome.browserAction.setBadgeBackgroundColor({color: '#0000FF', tabId: tabId}, () => {});
+  chrome.action.setBadgeText({tabId: tabId, text: reqCount.toString()});
+  chrome.action.setBadgeBackgroundColor({color: '#0000FF', tabId: tabId});
 
-	// Отправить новые данные запроса в popup
-	connection?.postMessage({msg: 'requests', data: [bodyObj]});
+  if(connection) {
+    connection.postMessage({msg: 'requests', data: [bodyObj]});
+  }
 }
-
-})();
